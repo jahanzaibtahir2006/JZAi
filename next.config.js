6f640ae9f245724d5072c9c05bec50d8c2de54a3
Jahanzaib Tahir <86833309+jahanzaibtahir2006@users.noreply.github.com>
2026-05-07 13:23:00 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['websitelaunches.com'],
     unoptimized: true,
   },
-  output: 'export',
 }
 module.exports = nextConfig
