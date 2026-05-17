7dd36c167fa1a174141f047aa968c07916e6ec3b
Jahanzaib Tahir <86833309+jahanzaibtahir2006@users.noreply.github.com>
2026-05-17 22:46:53 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Hero from '@/components/Hero'
 import Marquee from '@/components/Marquee'
 import Services from '@/components/Services'
 import Process from '@/components/Process'
-import TechStack from '@/components/TechStack'
-import WhyUs from '@/components/WhyUs'
 import FAQ from '@/components/FAQ'
 import CTA from '@/components/CTA'
 import Footer from '@/components/Footer'
@@ -17,9 +15,7 @@ export default function Home() {
       <Marquee />
       <Services />
       <Process />
-      <TechStack />
-      <WhyUs />
-      <FAQ /> 
+      <FAQ />
       <CTA />
       <Footer />
     </>
