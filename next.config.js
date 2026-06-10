7af42539bd39a87b40f242bf7a461e5f67239358
Jahanzaib Tahir <86833309+jahanzaibtahir2006@users.noreply.github.com>
2026-06-10 09:11:14 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,5 +5,27 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sandbox-cdn.paddle.com https://cdn.paddle.com",
+              "style-src 'self' 'unsafe-inline' fonts.googleapis.com https://sandbox-cdn.paddle.com https://cdn.paddle.com",
+              "style-src-elem 'self' 'unsafe-inline' fonts.googleapis.com https://sandbox-cdn.paddle.com https://cdn.paddle.com",
+              "font-src 'self' fonts.gstatic.com",
+              "img-src 'self' data: https:",
+              "frame-src 'self' https://sandbox-buy.paddle.com https://buy.paddle.com",
+              "connect-src 'self' https://sandbox-api.paddle.com https://api.paddle.com",
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
